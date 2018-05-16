4cd9e163e491893bbb9ffd7deb0e245b536906fd
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-05-16 16:45:59 -0700

    setup files and pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,30 @@
+var wordList = ['pizza', 'hamburger', 'french fries', 'pasta', 'fried rice', 'gyro']
+
+var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
+
+console.log(randomWord);
+
+
+//function ()
+    //initialize game score 
+
+//cpu will pick random word
+    //if user guess === indexof word.split (?)
+        //input correct guess into empty space at indexof word
+       
+    
+    //else 
+        //guesses --
+        //guessedLetters.push(key)
+ 
+    //if guesses ===  0
+        //alert
+        //losses++
+        //restart game
+        //choose new word
+    
+    //need to state a variable which will keep track of missing letters of the word. when variable reaches 0 
+        //alert
+        //wins++
+
+
