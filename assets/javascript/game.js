1be7f7a28eb56597f571f95717e0f42bd697d269
Albert Kim <lisa@Lisas-MacBook-Pro.local>
2018-05-17 21:14:26 -0700

    attempted homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,57 @@ var wordList = ['pizza', 'hamburger', 'french fries', 'pasta', 'fried rice', 'gy
 
 var randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 
-console.log(randomWord);
+var wins = 0;
+var losses = 0;
+var guesses = 7;
+var userGuesses = [];
+
+
+document.onkeydown = function(event) {
+    var key = event.key;
+
+    var playerGuess = key;
+        userGuesses.push(playerGuess);
+
+    var computerGuess = letterArray[Math.floor(Math.random()*letterArray.length)];
+
+    var win = document.getElementById("wins");
+    win.innerHTML = wins;
+
+    var lose = document.getElementById("losses");
+     lose.innerHTML = losses;
+
+    var guess = document.getElementById("guessLeft");
+     guess.innerHTML = guesses;
+    
+    var guessedLetters = document.getElementById('guesses');
+     guessedLetters.innerHTML = userGuesses.join(', ');
+
+    if (computerGuess === playerGuess) {
+        wins++;
+        guesses = 10;
+        userGuesses = [];
+        alert('You\'re psychic!');
+    } else if (guesses <= 1) {
+        losses ++;
+        guesses = 10;
+        userGuesses = [];
+        alert('You lose!');
+    } else if (computerGuess !== playerGuess) {
+        guesses--;
+    
+    }
+
+
+     
+}
+
+
+
+
+
+
+
 
 
 //function ()
@@ -23,7 +73,7 @@ console.log(randomWord);
         //restart game
         //choose new word
     
-    //need to state a variable which will keep track of missing letters of the word. when variable reaches 0 
+    // //need to state a variable which will keep track of missing letters of the word. when variable reaches 0 
         //alert
         //wins++
 
